2604fd1f88351e67f1e9dca8b59a284d1892b737
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-04-05 00:01:37 +0430

    String count unit test

---
+++ stringCount.js
@@ -0,0 +1,8 @@
+const stringCount = (string) => {
+  if (string.length >= 1 && string.length <= 10) {
+    return string.length;
+  } else {
+    throw new Error("The string arguments is at least 1 character long and not longer than 10 characters!");
+  }
+}
+module.exports = stringCount;
